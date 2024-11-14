fb27d89011e9fdda6c3d136be587170416ebabef
alamincse2003 <mdalamincse2003@gmail.com>
2024-11-14 17:04:21 +0600

    banner section design completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
-import './App.css';
-import NavBar from './components/NavBar';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Banner from "./components/Banner";
+import NavBar from "./components/NavBar";
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
+      <Banner></Banner>
     </div>
-  )
+  );
 }
 
 export default App;
